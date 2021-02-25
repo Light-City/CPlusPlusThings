799cc52db83fe784347df14f5e393532737b6df6
NishantRaizada-wq <70059378+NishantRaizada-wq@users.noreply.github.com>
2021-02-25 11:31:14 +0530

    Update blackclass.cpp
    This will make you the  use of the graphics including size

--- basic_content/sizeof/blackclass.cpp
+++ basic_content/sizeof/blackclass.cpp
@@ -7,12 +7,13 @@
  */
 
 #include<iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
-
+#include<graphics.h> // use size in graphics also
 class A{};
 int main()
 {
     cout<<sizeof(A)<<endl;
+    cout<<sizeof
     return 0;
 }
