9009f3b48022e944ac63bf7e4afd4611b1e284e6
Francis <455954986@qq.com>
2022-04-10 18:06:34 +0800

    Update main.cpp

--- basic_content/const/class_const/overload_example/main.cpp
+++ basic_content/const/class_const/overload_example/main.cpp
@@ -2,15 +2,21 @@
 #include"apple.cpp"
 using namespace std;
 const int Apple::apple_number=10;
-Apple::Apple(int i)
+#include<iostream>
+#include"apple.cpp"
+using namespace std;
+
+Apple::Apple(int i):apple_number(i)
 {
 
 }
-int Apple::add(int num){
-    take(num);
+int Apple::add(){
+    take(1);
+    return 0;
 }
 int Apple::add(int num) const{
     take(num);
+    return num;
 }
 void Apple::take(int num) const
 {
@@ -19,14 +25,14 @@ void Apple::take(int num) const
 int Apple::getCount() const
 {
     take(1);
-//    add(); //error
+    add();  // error
     return apple_number;
 }
 int main(){
     Apple a(2);
     cout<<a.getCount()<<endl;
     a.add(10);
-    const Apple b(3);
-    b.add(100);
+//     const Apple b(3);
+//     b.add(); // error
     return 0;
 }
