4afd3bab3890c5692ed7f9fdbb781e99b43789ab
Francis <455954986@qq.com>
2022-04-10 18:05:52 +0800

    Update apple.cpp

--- basic_content/const/class_const/overload_example/apple.cpp
+++ basic_content/const/class_const/overload_example/apple.cpp
@@ -6,7 +6,7 @@ public:
     Apple(int i);
     static const int apple_number;
     void take(int num) const;
-    int add(int num);
+    int add();
     int add(int num) const;
     int getCount() const;
 };
