2657e410aa8c9175ac89ac1d43b7e57c6a5a0f45
laialaodi <776273308@qq.com>
2023-02-28 20:40:27 +0800

    Format file

--- basic_content/func_pointer/func_pointer.cpp
+++ basic_content/func_pointer/func_pointer.cpp
@@ -12,22 +12,22 @@ using namespace std;
 /**
  * @brief 定义了一个变量pFun，这个变量是个指针，指向返回值为空和参数为int的函数的指针！
  */
-void (*pFun)(int); 
+void (*pFun)(int);
 
 /**
  * @brief 代表一种新类型，不是变量！所以与上述的pFun不一样！
  */
-typedef void (*func)(void);  
+typedef void (*func)(void);
 
 void myfunc(void)
 {
     cout<<"asda"<<endl;
 }
 
-void glFun(int a){ cout<<a<<endl;} 
+void glFun(int a){ cout<<a<<endl;}
 int main(){
-    func pfun = myfunc;/*赋值*/  
-    pfun();/*调用*/  
-    pFun = glFun; 
-    (*pFun)(2); 
+    func pfun = myfunc;/*赋值*/
+    pfun();/*调用*/
+    pFun = glFun;
+    (*pFun)(2);
 }
